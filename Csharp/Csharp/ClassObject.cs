85cfd221e05299e0cf1699814a2119c294e4447b
Arjun <arjunmavadiya27990@gmail.com>
2025-09-25 18:04:59 +0530

    File Upload

---
+++ Csharp/Csharp/ClassObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    internal class ClassObject
+    {
+        public string Brand;
+        public string Model;
+        public int Year;
+
+        public ClassObject(string brand, string model, int year)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+        }
+
+        public void Multiple()
+        {
+            Console.WriteLine($"Brand: {Brand}, Model: {Model}, Year: {Year}");
+        }
+    }
+}
