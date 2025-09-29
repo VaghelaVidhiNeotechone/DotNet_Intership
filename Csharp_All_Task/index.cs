2eec4915cdb20f9961f0a35753d5b4a2cc6a4748
VaghelaVidhiNeotechone <vidhiv@niotechone.com>
2025-09-29 14:59:23 +0530

    update index

--- Csharp_All_Task/index.cs
+++ Csharp_All_Task/index.cs
@@ -12,22 +12,22 @@ namespace Csharp_All_Task
         {
 
                 Console.WriteLine("***************** Day - 1(Datatype_Variable) ******************");
-                Datatype_Variable.run();
+                //Datatype_Variable.run();
                 Console.WriteLine("***************** Day - 2(String_Conditional) ******************");
-                String_Conditional.run();
+                //String_Conditional.run();
                 Console.WriteLine("***************** Day - 3(Loops_Method) ******************");
-                Loops_Method.run();
+                //Loops_Method.run();
                 Console.WriteLine("***************** Day - 4(Class_Object) ******************");
-                Class_Object.car();
+                //Class_Object.car();
                 Console.WriteLine("***************** Day - 5(Constructor_Encapculation) ******************");
-                Constructor_Encapculation.run();
+                //Constructor_Encapculation.run();
                 Console.WriteLine("***************** Day - 6(Inheritace_Polymorphism) ******************");
-                Inheritace_Polymorphism.run();
+                //Inheritace_Polymorphism.run();
                 Console.WriteLine("***************** Day - 7(Abstract_Interface_Array) ******************");
-                Abstract_Interface_Array.run();
+                //Abstract_Interface_Array.run();
                 Console.WriteLine("***************** Day - 8(LeetCode_Practice) ******************");
                 TwoSumNum.run();
-                Longest_Common_Prefix.run();
+                //Longest_Common_Prefix.run();
 
 
         }
