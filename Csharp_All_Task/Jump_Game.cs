c56147cf9c46a4e6203d96c968b590322cb806b2
VaghelaVidhiNeotechone <vidhiv@niotechone.com>
2025-10-15 15:11:08 +0530

    Add

---
+++ Csharp_All_Task/Jump_Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_All_Task
+{
+    internal class Jump_Game
+    {
+        public bool CanJump(int[] nums)
+        {
+            int finishIndex = nums.Length - 1;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (i + nums[i] >= finishIndex)
+                {
+                    if (i == 0) return true;
+                    finishIndex = i;
+                }
+            }
+            return false;
+        }
+        public static void run()
+        {
+            Jump_Game obj = new Jump_Game();
+            Console.WriteLine("---- Jump_Game ----");
+            Console.WriteLine("Enter Element of Array(space Seperate):");
+            int[] nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            bool result = obj.CanJump(nums);
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
