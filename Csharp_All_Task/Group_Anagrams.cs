def9b151d2878cde96a2f95240f0e04cf131b12d
VaghelaVidhiNeotechone <vidhiv@niotechone.com>
2025-10-15 10:10:50 +0530

    Modify

--- Csharp_All_Task/Group_Anagrams.cs
+++ Csharp_All_Task/Group_Anagrams.cs
@@ -6,24 +6,19 @@ namespace Csharp_All_Task
 {
     internal class Group_Anagrams
     {
-        // New approach: use character frequency key instead of sorting
         public IList<IList<string>> GroupAnagrams(string[] strs)
         {
             Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();
 
             foreach (string word in strs)
             {
-                // Create frequency array of size 26 (for lowercase letters a-z)
                 int[] count = new int[26];
                 foreach (char c in word)
                 {
                     count[c - 'a']++;
                 }
-
-                // Create a unique key like "1#0#0#2#..." to represent frequency
                 string key = string.Join("#", count);
 
-                // Group words with same key together
                 if (!map.ContainsKey(key))
                     map[key] = new List<string>();
 
@@ -33,7 +28,7 @@ namespace Csharp_All_Task
             return map.Values.ToList<IList<string>>();
         }
 
-        // Updated and more user-friendly run() method
+ 
         public static void run()
         {
             Group_Anagrams obj = new Group_Anagrams();
