a34e7761b9a95b879b6e83ec968a370b4b2d88a0
VaghelaVidhiNeotechone <vidhiv@niotechone.com>
2025-10-28 11:47:13 +0530

    Add

---
+++ .NET(Yashvi)/New folder/MvcDay1Demo/MvcDay1Demo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDay1Demo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
