56d3c52c8536624aa5401bc1e9522c692da76c8d
VaghelaVidhiNeotechone <vidhiv@niotechone.com>
2025-10-30 15:43:11 +0530

    ADD Folder

---
+++ .NET(Yashvi)/StudentFormMVC/StudentFormMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentFormMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ .NET(Yashvi)/StudentFormMVC/StudentFormMVC/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentFormMVC.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Course is required")]
+        public string Course { get; set; }
+
+        [Range(18, 60, ErrorMessage = "Age must be between 18 and 60")]
+        public int Age { get; set; }
+    }
+}
