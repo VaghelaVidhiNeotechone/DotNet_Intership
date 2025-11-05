3d56d11d4520f2eddb737c9c81e3553ce57ff129
Arjun <arjunmavadiya27990@gmail.com>
2025-11-05 19:29:45 +0530

    Add

---
+++ SQL Backup Files(Arjun)/Exercise/Department Top Three Salaries/Department Top Three Salaries.sql
@@ -0,0 +1,42 @@
+create table Department
+(
+id int primary key,
+name varchar(50)
+);
+
+insert into Department values
+(1,'IT'),
+(2,'Sales');
+
+select * from Department;
+
+
+create table Emp
+(
+id int primary key,
+name varchar(50),
+Salary int,
+departmentId int
+);
+
+insert into Emp values
+(1,'Joe',70000,1),
+(2,'Jim',90000,1),
+(3,'Henry',80000,2),
+(4,'Sam',60000,2),
+(5,'Max',90000,1);
+
+select * from Emp;
+
+
+
+SELECT d.name AS Department,e.name AS Emp,e.salary AS Salary FROM Emp e
+JOIN Department d ON e.departmentId = d.id
+WHERE 
+    (
+        SELECT COUNT(DISTINCT e2.salary)
+        FROM Emp e2
+        WHERE e2.departmentId = e.departmentId
+          AND e2.salary > e.salary
+    ) < 3
+ORDER BY d.name,e.salary DESC;
