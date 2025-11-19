b3490a0d1ee9808e8323f9ff13f682065168319f
Arjun <arjunmavadiya27990@gmail.com>
2025-11-19 16:03:30 +0530

    Add File

---
+++ Dotnet (Arjun)/Day Task/JwtAuthDemo/JwtAuthDemo/Controllers/DataController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet("admin")]
+        [Authorize(Policy = "AdminOnly")]
+        public IActionResult GetAdminData()
+        {
+            return Ok("This data is for **Admin**.");
+        }
+
+        [HttpGet("user")]
+        [Authorize(Policy = "UserOnly")]
+        public IActionResult GetUserData()
+        {
+            return Ok("This data is for **User**.");
+        }
+
+        [HttpGet("all")]
+        [Authorize]
+        public IActionResult GetAllAuthenticated()
+        {
+            return Ok("This data can be viewed by any user with a valid token.");
+        }
+    }
+}
