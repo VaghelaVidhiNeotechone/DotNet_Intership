ffa328738616a557c38c641cc16e86547f2ef938
Arjun <arjunmavadiya27990@gmail.com>
2025-12-04 18:36:52 +0530

    Add CRUD With WebAPI

---
+++ Dotnet (Arjun)/Day Task/Implement Login functionality with WebAPI/CRUD with WebAPI/CRUD with WebAPI/DTOs/CreateStudentRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_with_WebAPI.DTOs
+{
+    public class CreateStudentRequestDTO
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name should be max 50 characters.")]
+        public string? Name { get; set; }
+
+        [Range(18, 60, ErrorMessage = "Age should be between 18 to 60")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        public string? City { get; set; }
+    }
+}
---
+++ Dotnet (Arjun)/Day Task/Implement Login functionality with WebAPI/CRUD with WebAPI/CRUD with WebAPI/DTOs/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CRUD_with_WebAPI.DTOs
+{
+    public class LoginRequestDTO
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
