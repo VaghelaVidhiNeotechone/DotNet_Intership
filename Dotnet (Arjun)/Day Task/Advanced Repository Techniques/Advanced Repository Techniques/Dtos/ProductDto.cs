e42a9164b2d6cdce44c645586f4904e60e8a937b
Arjun <arjunmavadiya27990@gmail.com>
2025-12-15 19:14:53 +0530

    .Net #Task info 160 & 163

---
+++ Dotnet (Arjun)/Day Task/Advanced Repository Techniques/Advanced Repository Techniques/Dtos/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Advanced_Repository_Techniques.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
---
+++ Dotnet (Arjun)/Day Task/Advanced Repository Techniques/Advanced Repository Techniques/Models/SoftDeleteEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Advanced_Repository_Techniques.Models
+{
+    public abstract class SoftDeleteEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
+    }
+}
