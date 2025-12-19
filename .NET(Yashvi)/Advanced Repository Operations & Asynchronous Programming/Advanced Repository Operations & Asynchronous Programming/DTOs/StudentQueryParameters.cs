df82c200bf3e76fe52c457988d0740b4697c8a87
VaghelaVidhiNeotechone <vidhiv@niotechone.com>
2025-12-19 18:31:44 +0530

    Add Folder

---
+++ .NET(Yashvi)/Advanced Repository Operations & Asynchronous Programming/Advanced Repository Operations & Asynchronous Programming/DTOs/StudentQueryParameters.cs
@@ -0,0 +1,10 @@
+﻿public class StudentQueryParameters
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 5;
+
+    public string? Department { get; set; }
+
+    public string? SortBy { get; set; } // name, marks
+    public bool IsDescending { get; set; } = false;
+}
---
+++ .NET(Yashvi)/Advanced Repository Operations & Asynchronous Programming/Advanced Repository Operations & Asynchronous Programming/Repositories/Interfaces/IStudentRepository.cs
@@ -0,0 +1,8 @@
+﻿public interface IStudentRepository
+{
+    Task<IEnumerable<Student>> GetAllAsync(StudentQueryParameters parameters);
+    Task<Student?> GetByIdAsync(int id);
+    Task AddAsync(Student student);
+    Task UpdateAsync(Student student);
+    Task DeleteAsync(int id);
+}
