3c4d109d784110a869345982e44a5ac045d812e2
VaghelaVidhiNeotechone <vidhiv@niotechone.com>
2025-12-24 19:04:10 +0530

    Add Files

---
+++ .NET(Yashvi)/Advanced Repository Techniques/Advanced Repository Techniques/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+    public bool IsDeleted { get; set; } = false;
+    public DateTime? DeletedAt { get; set; }
+}
