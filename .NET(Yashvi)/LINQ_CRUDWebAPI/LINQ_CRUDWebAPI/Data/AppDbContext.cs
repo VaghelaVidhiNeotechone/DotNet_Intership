07fd112778a9e9d566c8d93e007bb3d640330c32
VaghelaVidhiNeotechone <vidhiv@niotechone.com>
2025-12-29 19:17:59 +0530

    Add Folder

---
+++ .NET(Yashvi)/LINQ_CRUDWebAPI/LINQ_CRUDWebAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LINQ.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LINQ.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Order> Orders => Set<Order>();
+    }
+}
---
+++ .NET(Yashvi)/LINQ_CRUDWebAPI/LINQ_CRUDWebAPI/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace LINQ.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal TotalAmount { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+    }
+}
